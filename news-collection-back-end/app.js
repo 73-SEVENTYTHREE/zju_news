59e65dd857df6d92a2f8c51c3591921fddf19ce6
73-SEVENTYTHREE <2569535507@qq.com>
2021-09-20 21:32:01 +0800

    update app

--- news-collection-back-end/app.js
+++ news-collection-back-end/app.js
@@ -6,4 +6,4 @@ const { Worker } = require('worker_threads');
 const th = new Worker(__dirname + '/updateData.js');
 app.use(express.json())
 app.use("/", router);
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(port, () => console.log(`已监听在4000端口`));
